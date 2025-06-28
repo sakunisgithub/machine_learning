6e3c655ec30eae43dc0465e2e916648b46a86912
sakunisgithub <ananda2003biswas@gmial.com>
2025-06-28 17:29:57 +0530

    added comments

--- Deep Learning - IIT Ropar/003_gradient_descent.py
+++ Deep Learning - IIT Ropar/003_gradient_descent.py
@@ -3,14 +3,20 @@ import numpy as np
 X = np.array([0.5, 2.5])
 Y = np.array([0.2, 0.9])
 
+# prediction equation -- sigmoid function
+
 def f(x, w, b) :
     return(1/(1 + np.exp(-(w*x + b))))
+
+# derivatives of Squared Error Loss function w.r.t. the parameters
 
 def dw(X, Y, w, b) :
     return( np.sum( (f(X, w, b) - Y) * f(X, w, b) * (1 - f(X, w, b)) * X ) )
 
 def db(X, Y, w, b) :
     return( np.sum( (f(X, w, b) - Y) * f(X, w, b) * (1 - f(X, w, b)) ) )
+
+# gradient descent
 
 def gradient_descent(X, Y, w_initial, b_initial, eta, max_epochs) :
 
@@ -21,6 +27,8 @@ def gradient_descent(X, Y, w_initial, b_initial, eta, max_epochs) :
         b = b - eta * db(X, Y, w, b)
 
     return((w, b))
+
+# doing gradient descent
 
 w, b = gradient_descent(X, Y, 1, 1, 1, 1000)
 
